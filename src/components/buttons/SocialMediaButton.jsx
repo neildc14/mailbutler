82ee705603ed8d03b3dca92337182f7da0bba012
neil dela cruz <genoznightmare@gmail.com>
2022-11-21 15:26:32 +0800

    desktop footer

--- src/components/buttons/SocialMediaButton.jsx
+++ src/components/buttons/SocialMediaButton.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SocialIcon } from "react-social-icons";
 
 function SocialMediaButton({ url }) {
+  const [hover, setHover] = useState(false);
+
+  const hoverLink = () => {
+    setHover(!hover);
+  };
+
+  let bg_color = "#51A387";
+
+  if (hover) {
+    bg_color = "#276749";
+  }
   return (
-    <SocialIcon url={url} bgColor="#51A387" style={{ height: 40, width: 40 }} />
+    <SocialIcon
+      url={url}
+      bgColor={bg_color}
+      style={{ height: 40, width: 40 }}
+      onMouseEnter={hoverLink}
+      onMouseLeave={hoverLink}
+    />
   );
 }
 
